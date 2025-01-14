c661adac0035c9e97380eeae4f9da690bc7a14b5
wenping <wenping@meituan.com>
2025-01-14 18:53:11 +0800

    refactor: improve logic

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-console.log('Welcome to the Number Guessing Game!');
-console.log("I'm thinking of a number between 1 and 100.");
-console.log('You have 5 chances to guess the correct number.');
-
-console.log('Please select the difficulty level:');
-console.log('1. Easy 10 (chances)');
-console.log('2. Medium 5 (chances)');
-console.log('3. Hard 3 (chances)');
-
 import readline from 'readline';
+
+let level;
+let startTime;
+let chances;
+let randomNumber;
+let attempts;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -26,6 +23,74 @@ const chancesMap = {
   3: 3,
 };
 
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+async function gameIntro() {
+  console.log('Welcome to the Number Guessing Game!');
+  console.log("I'm thinking of a number between 1 and 100.");
+  console.log('You have 5 chances to guess the correct number.');
+  console.log('Please select the difficulty level:');
+  console.log('1. Easy 10 (chances)');
+  console.log('2. Medium 5 (chances)');
+  console.log('3. Hard 3 (chances)');
+}
+
+async function tryAgain() {
+  const answer = await anotherRound();
+  if (answer === true) {
+    startTime = Date.now();
+    chances = chancesMap[level];
+    randomNumber = generateRandomNumber();
+    attempts = 0;
+    console.clear();
+    startGame();
+  } else {
+    console.log('Thank you for playing!');
+  }
+}
+async function startGame() {
+  gameIntro();
+  while (true) {
+    try {
+      level = await getInput();
+      break;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  console.log(
+    `Great! You have selected the ${levelMap[level]} difficulty level.\nLet's start the game!`
+  );
+  startTime = Date.now();
+  chances = chancesMap[level];
+  randomNumber = generateRandomNumber();
+  attempts = 0;
+
+  while (chances > 0) {
+    let guess = await getRandomNumber();
+    attempts++;
+    if (guess === randomNumber) {
+      const endTime = Date.now();
+      const timeTaken = endTime - startTime;
+      console.log(
+        `Congratulations! You guessed the correct number in ${attempts} attempts.`
+      );
+      console.log(`Time taken: ${timeTaken / 1000} seconds`);
+      await tryAgain();
+    } else if (guess < randomNumber) {
+      console.log(`Incorrect! You have to guess higher than ${guess}.`);
+    } else {
+      console.log(`Incorrect! You have to guess lower than ${guess}.`);
+    }
+    chances--;
+    if (chances === 0) {
+      await tryAgain();
+    }
+  }
+}
+
 async function getAnswer(prompt) {
   return new Promise((resolve) => {
     rl.question(prompt, (answer) => {
@@ -34,11 +99,11 @@ async function getAnswer(prompt) {
   });
 }
 async function getInput() {
-  const choice = await getAnswer('Enter your choice: ');
+  let choice = await getAnswer('Enter your choice: ');
   if (levelMap[choice]) {
     return choice;
   }
-  console.log('not a valid choice!! please choice again!!');
+  throw new Error('not a valid choice!! please choice again!!');
 }
 
 async function getRandomNumber() {
@@ -54,57 +119,6 @@ async function anotherRound() {
   return false;
 }
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 100) + 1;
-}
-
-let level;
-while (true) {
-  try {
-    level = await getInput();
-    break;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-console.log(
-  `Great! You have selected the ${levelMap[level]} difficulty level.\nLet's start the game!`
-);
-
-const startTime = Date.now();
-let chances = chancesMap[level];
-let randomNumber = generateRandomNumber();
-let attempts = 0;
-while (chances > 0) {
-  let guess = await getRandomNumber();
-  attempts++;
-  if (guess === randomNumber) {
-    const endTime = Date.now();
-    const timeTaken = endTime - startTime;
-    console.log(
-      `Congratulations! You guessed the correct number in ${attempts} attempts.`
-    );
-    console.log(`Time taken: ${timeTaken / 1000} seconds`);
-    break;
-  } else if (guess < randomNumber) {
-    console.log(`Incorrect! You have to guess higher than ${guess}.`);
-  } else {
-    console.log(`Incorrect! You have to guess lower than ${guess}.`);
-  }
-  chances--;
-  if (chances === 0) {
-    const answer = await anotherRound();
-    if (answer === true) {
-      startTime = Date.now();
-      chances = chancesMap[level];
-      randomNumber = generateRandomNumber();
-      attempts = 0;
-    } else {
-      console.log('Thank you for playing!');
-      break;
-    }
-  }
-}
+await startGame();
 
 rl.close();
